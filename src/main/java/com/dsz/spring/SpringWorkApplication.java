6b725afbf58fd718d57d19231d4144fa25e9bb09
段守志 <dszazhy1314@163.com>
2022-08-12 11:51:04 +0800

    实现ws文件上传

--- src/main/java/com/dsz/spring/SpringWorkApplication.java
+++ src/main/java/com/dsz/spring/SpringWorkApplication.java
@@ -1,17 +1,17 @@
 package com.dsz.spring;
 
+import com.dsz.spring.code_test.FinalTest;
 import com.dsz.spring.config.MyConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ObjectUtils;
 
-import javax.imageio.stream.ImageInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 
+@Slf4j
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringWorkApplication {
 
@@ -19,14 +19,12 @@ public class SpringWorkApplication {
         ConfigurableApplicationContext run = SpringApplication.run(SpringWorkApplication.class, args);
         MyConfig config = run.getBean(MyConfig.class);
         System.out.println(config.getName());
-        //模拟多线程操作
-//        int i = 0;
-//        while (i < 100) {
-//            i++;
-//            new Thread(()->{
-//                System.out.println(Thread.currentThread().getName());
-//            }).start();
-//        }
+
+        // TEST final params
+        log.info("test final ......");
+        //log.info("test static" + FinalTest.staticA);
+        log.info("test static" + FinalTest.staticFinalA);
+        ObjectUtils.isEmpty(null);
     }
 
 }
---
+++ src/main/java/com/dsz/spring/code_test/FinalTest.java
@@ -0,0 +1,13 @@
+package com.dsz.spring.code_test;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Date: 2022/8/11 11:43
+ * @Description:
+ */
+public class FinalTest {
+    // TODO 1、装载 2、验证 3、准备 4、解析 5、初始化 6、使用 7、卸载
+    public static int staticA = 666;
+    public final static int staticFinalA = 888;
+
+}
